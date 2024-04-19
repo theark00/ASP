7d72f1c5fa1a7131e6e0e655dfd48ea525c745f0
theark00 <kevinark00@gmail.com>
2024-04-19 10:58:45 +0200

    Part 1

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CoursesController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok();
+    }
+}
